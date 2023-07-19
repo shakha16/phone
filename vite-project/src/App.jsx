6dad07c220a5542d003c9d0d5d785ff223413089
Shoxrux <ganiorzikulov@gmail.com>
2023-07-19 13:42:37 +0500

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function App() {
+
+    return (
+        <>
+            <div className="container">
+                <img src="https://images.uzum.uz/cir7njd6sfhndlbqkk10/main_page_banner.jpg" alt="" />
+            </div>
+        </>
+    )
+}
+
+export default App
